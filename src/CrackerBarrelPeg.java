90fd5cba98caaac6b298551387a16f8e44a222be
Anitha Shivaramaiah <anitha22shivaram@gmail.com>
2022-05-03 14:31:31 -0500

    Merge remote-tracking branch 'origin/master'

--- src/CrackerBarrelPeg.java
+++ src/CrackerBarrelPeg.java
@@ -2,92 +2,81 @@ class CrackerBarrelPeg
 {
     public static void main(String[] args)
     {
-        Puzzle(0);
+        Game(0);
     }
 
     public static Movement play(Movement mov, Play p)
     {
-        if (mov.board[p.from] == 1 && mov.board[p.over] == 1 && mov.board[p.to] == 0)
-        {
-            mov.flag1 = true;
-            mov.board[p.from] = 0;
-            mov.board[p.over] = 0;
-            mov.board[p.to] = 1;
-            mov.counter -= 1;
-            return mov;
-        }
-        else
+        if(mov.board[p.from] != 1 && mov.board[p.over] != 1 && mov.board[p.to] != 0)
         {
             mov.flag1 = false;
             return mov;
         }
+        else
+        {
+            mov.counter -= 1;
+            mov.flag1 = true;
+            mov.board[p.from] = 0;
+            mov.board[p.over] = 0;
+            mov.board[p.to] = 1;
+
+            return mov;
+
+        }
     }
 
-//        if (mov.board[p.from] == 1)
-//        {
-//            if (mov.board[p.over] == 1)
-//            {
-//                if (mov.board[p.to] == 0)
-//                {
-//                    mov.flag1 = true;
-//                    mov.board[p.from] = 0;
-//                    mov.board[p.over] = 0;
-//                    mov.board[p.to] = 1;
-//                    mov.counter -= 1;
-//                    return mov;
-//                }
-//            }
-//        }
-//        else
-//        {
-//            mov.flag1 = false;
-//            return mov;
-//        }
-//    }
-
-
-    public static void solution(Movement mov, boardMovement bm)
+    public static void solve(Movement mov, BoardMovement bm)
     {
-        for (int i = 0; i < bm.counter; i++)
+
+        int i = 0;
+        while(i<bm.counter)
         {
-            Movement newMov = new Movement(mov);
-            Play temporary = bm.nextMove(i);
-            newMov = play(newMov, temporary);
-            if (newMov.flag1)
+            Movement movNew = new Movement(mov);
+            Play temporary = bm.getMove(i);
+            movNew = play(movNew, temporary);
+            i++;
+
+            if(movNew.flag1)
             {
-                newMov.againP[newMov.again] = temporary;
-                newMov.again = newMov.again + 1;
-                if (newMov.counter < 2)
+                movNew.againP[movNew.again] = temporary;
+                movNew.again = movNew.again + 1;
+
+                if(movNew.counter > 2)
                 {
-                    printing(newMov);
+                    solve(movNew, bm);
+
                 }
-                solution(newMov, bm);
+                printing(movNew);
             }
         }
     }
 
-    public static void printing(Movement mov) {
-        mov.boardRestart();
+    public static void printing(Movement mov)
+    {
+        mov.restartGame();
         mov.print();
+
         int i = 0;
-        while (i < mov.again) {
+        while(i < mov.again)
+        {
             mov = play(mov, mov.againP[i]);
             mov.print();
             i++;
         }
-        Puzzle(mov.startPos + 1);
+        Game(mov.startPos + 1);
+
     }
 
-    public static void Puzzle(int startPos)
+    public static void Game(int startPos)
     {
-        if (startPos > 5)
+        if(startPos > 5)
         {
             System.exit(0);
         }
-        System.out.println("==" + startPos + "==");
-        boardMovement bm = new boardMovement();
+        System.out.println("=== " + startPos + " ===");
+        BoardMovement bm = new BoardMovement();
         Movement mov = new Movement(startPos);
-        solution(mov, bm);
+        solve(mov, bm);
     }
 }
 
@@ -110,12 +99,12 @@ class Play
     }
 }
 
-class boardMovement
+class BoardMovement
 {
     public Play boardMovement[] = new Play[36];
     public int counter;
 
-    public boardMovement()
+    public BoardMovement()
     {
         boardMovement[0] = new Play(0,1,3);
         boardMovement[1] = new Play(0,2,5);
@@ -137,82 +126,79 @@ class boardMovement
         boardMovement[17] = new Play(12,13,14);
 
         int j = 18;
+        // for(int i = 0; i < 18; i++)
         int i = 0;
         while(i < 18)
         {
             boardMovement[j] = new Play(boardMovement[i].to, boardMovement[i].over, boardMovement[i].from);
-            j++;
-            i++;
+            i++; j++;
         }
+
         counter = 36;
     }
 
-    public Play nextMove(int i)
+    public Play getMove(int i)
     {
         return boardMovement[i];
     }
 }
-
 class Movement
 {
-    public int counter;
     public int board[] = new int[15];
-    public int startPos;
-    public int i = 0;
-    public int again = 0;
-    public Play againP[] = new Play[2000];
+    public int counter;
     public boolean flag1 = false;
-
-    public void boardRestart()
+    public Play againP[] = new Play[1000];
+    public int again = 0;
+    public int startPos;
+    public Movement(int s)
     {
-        while(i < 15)
+        for(int i = 0; i < 15; i++)
         {
             board[i] = 1;
-            i++;
         }
-        board[startPos] = 0;
-    }
-
-    public Movement(int start)
-    {
-        i = 0;
-        while(i<15)
-        {
-            board[i] = 1;
-            i++;
-        }
-        startPos = start;
-        board[start] = 0;
+        startPos = s;
+        board[s] = 0;
         counter = 14;
+
     }
+
     public Movement(Movement mov)
     {
-        int i = 0;
-        while(i<15)
+        for(int i = 0; i < 15; i++)
         {
             board[i] = mov.board[i];
-            i++;
+
         }
-        while(i< mov.again)
+        for(int i = 0; i < mov.again; i++)
+
         {
             againP[i] = mov.againP[i];
-            i++;
         }
         flag1 = false;
         startPos = mov.startPos;
         counter = mov.counter;
         again = mov.again;
+
     }
 
+    public void restartGame()
+    {
+        for(int i = 0; i < 15; i++)
+
+        {
+            board[i] = 1;
+
+        }
+        board[startPos] = 0;
+    }
 
     public void print()
     {
-        System.out.println("    "+ board[0]);
-        System.out.println("   "+ board[1]+ " " +board[2]);
+        System.out.println("    " + board[0]);
+        System.out.println("   " + board[1] + " " + board[2]);
         System.out.println("  " + board[3] + " " + board[4] + " " + board[5]);
         System.out.println(" " + board[6] + " " + board[7] + " " + board[8] + " " + board[9]);
         System.out.println(board[10] + " " + board[11] + " " + board[12] + " " + board[13] + " " + board[14]);
         System.out.print("\n");
     }
 }
-
